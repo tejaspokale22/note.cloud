af31ee56210cd967a7ec2380cacc993833e8258b
tejaspokale22 <tejaspokale22@gmail.com>
2025-07-13 16:40:45 +0530

    removed middleware for getNotes

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -12,7 +12,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // Define routes and use the controller functions with authentication
-router.get("/", authMiddleware, getNotes);
+router.get("/", getNotes);
 router.get("/:id", authMiddleware, getNoteById);
 router.post("/", authMiddleware, createNote);
 router.put("/:id", authMiddleware, updateNote);
