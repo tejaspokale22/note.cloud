bf2895efd8ac05cca3fb4516b5aa169bcfa3c081
tejaspokale22 <tejaspokale22@gmail.com>
2025-07-30 01:23:51 +0530

    feat: added rate limiter

--- Backend/server.js
+++ Backend/server.js
@@ -4,21 +4,25 @@ import cors from "cors";
 import dotenv from "dotenv";
 import notesRoutes from "./routes/notes.js";
 import authRoutes from "./routes/auth.js";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Middleware for CORS
-// app.use(
-//   cors({
-//     origin: "https://mynotescloud.vercel.app/",
-//   })
-// );
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
+
+// Rate limiter middleware
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  max: 4, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+
+// Apply it globally
+app.use(limiter);
 
 // Connect to MongoDB
 mongoose
